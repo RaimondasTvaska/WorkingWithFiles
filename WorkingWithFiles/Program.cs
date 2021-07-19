2fef4844ff036752e616e9792344dda6c98f12e9
RaimondasTvaska <rtvaska@gmail.com>
2021-07-19 18:41:53 +0300

    Add project files.

---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("WrittenFile.txt", "Hello everyone");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
